f70e0122fed7431c4c177921cc5067ba9b3e85e3
Edgar Delgado <delga157@mail.chapman.edu>
2020-08-15 21:51:37 -0700

    Implemented TableView

--- TopMusicAlbums/ViewController.swift
+++ TopMusicAlbums/ViewController.swift
@@ -9,14 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let tableView = UITableView()
+    var safeArea = UILayoutGuide()
+    var sampleData = ["Dodgers", "Angels", "Rangers", "Yankees"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("TESTing")
+        view.backgroundColor = .white
+        safeArea = view.layoutMarginsGuide
+        
+        tableView.dataSource = self
+        setupTableView()
     }
 
-
+    func setupTableView() {
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = sampleData[indexPath.row]
+        return cell
+    }
+}
